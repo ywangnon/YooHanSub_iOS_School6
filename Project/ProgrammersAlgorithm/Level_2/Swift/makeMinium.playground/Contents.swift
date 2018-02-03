50daec560c8cc20145d488d82c392b8f8a27ea16
ywangnon <ywangnon123@gmail.com>
2018-02-04 01:29:44 +0900

    addReadMe

---
+++ Project/ProgrammersAlgorithm/Level_2/Swift/makeMinium.playground/Contents.swift
@@ -0,0 +1,11 @@
+func getMinSum(A: [Int], B: [Int]) {
+    var answer = 0;
+    
+    return answer;
+}
+
+//아래 코드는 테스트를 위한 출력 코드 입니다.
+var tA = [1,2],
+var tB = [3,4];
+
+print(getMinSum(A: tA,B: tB));
