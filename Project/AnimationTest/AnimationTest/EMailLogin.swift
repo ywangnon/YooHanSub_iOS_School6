3a71922c2b8ddbad0d91a8c048f4e82a9a66e154
ywangnon <ywangnon123@gmail.com>
2018-04-06 22:02:05 +0900

    4.6

---
+++ Project/AnimationTest/AnimationTest/EMailLogin.swift
@@ -0,0 +1,39 @@
+//
+//  EMailLogin.swift
+//  AnimationTest
+//
+//  Created by Hansub Yoo on 2018. 4. 6..
+//  Copyright © 2018년 hansub yoo. All rights reserved.
+//
+
+import UIKit
+
+class EMailLogin: UIViewController {
+
+    @IBAction func cancelAction(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
